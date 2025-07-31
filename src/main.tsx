e265c019f5198f5cca6084c75c1396cc8e9f2c8e
petebutland <pbutland@protonmail.com>
2025-07-31 15:34:50 +1000

    Added config for leaflet images

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import L from 'leaflet';
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: '/marker-icon-2x.png',
+  iconUrl: '/marker-icon.png',
+  shadowUrl: '/marker-shadow.png',
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
